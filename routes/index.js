cc17afa147115c09c51bd85158d00013624404c9
irfan <engr.irfandayan@gmail.com>
2022-08-21 14:15:36 +0500

    Add index route

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,9 @@ const router = express.Router();
 // Routes
 // home page
 router.get("/", async (req, res) => {
-  res.send(`<h1>Please use postman to make requests to get posts</h1>`);
+  res.send(
+    `<h1>This is an API so please use postman to make requests to get posts</h1>`
+  );
 });
 
 export default router;
